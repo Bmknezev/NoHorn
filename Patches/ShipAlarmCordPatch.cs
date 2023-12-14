bf51dbc9d05038d27746e1e6c5f89e43d84d15b1
Braydon <bmknezev@lakeheadu.ca>
2023-12-13 21:50:42 -0500

    Add project files.

---
+++ Patches/ShipAlarmCordPatch.cs
@@ -0,0 +1,41 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoHorn.Patches
+{
+    [HarmonyPatch(typeof(ShipAlarmCord))]
+    internal class ShipAlarmCordPatch
+    {
+        private static Random random = new Random();
+        [HarmonyPatch("Update")]
+        [HarmonyPrefix]
+        static void hornCheckPatch(ref bool ___localClientHoldingCord)
+        {
+            if (___localClientHoldingCord)
+            {
+
+                if (random.Next(0,1000) == 2)
+                {
+                    PlayerControllerB player = StartOfRound.Instance.localPlayerController;
+                    player.DamagePlayer(100, hasDamageSFX: true, callRPC: true, CauseOfDeath.Unknown);
+                }
+            }
+
+        }
+        /*
+        [HarmonyPatch("HoldCordDown")]
+        [HarmonyPrefix]
+        public static bool Prefix()
+        {
+            PlayerControllerB player = StartOfRound.Instance.localPlayerController;
+            player.DamagePlayer(100, hasDamageSFX: true, callRPC: true, CauseOfDeath.Unknown);
+            return false;
+        }
+        */
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,45 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using NoHorn.Patches;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoHorn
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class NoHornBase : BaseUnityPlugin
+    {
+        private const string modGUID = "Poseidon.NoHorn";
+        private const string modName = "No Loud Horn";
+        private const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static NoHornBase Instance;
+
+        internal ManualLogSource mls;
+
+
+
+        void Awake()
+        {
+            if (Instance == null)
+                Instance = this;
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo("The No Horn mod is online");
+
+            harmony.PatchAll(typeof(NoHornBase));
+            //harmony.PatchAll(typeof(PlayerControllerBPatch));
+            harmony.PatchAll(typeof(ShipAlarmCordPatch));
+
+        }
+
+
+    }
+}
